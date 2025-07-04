0cde496e8d0c96c2e35f61573b34e5f0a67a0f9c
Simon Cozens <simon@simon-cozens.org>
2025-07-04 12:17:44 +0100

    chore: More error handling, save a designspace file

--- fontspector-checkapi/src/source.rs
+++ fontspector-checkapi/src/source.rs
@@ -1,14 +1,64 @@
-use std::ffi::OsStr;
-use std::path::{Path, PathBuf};
+use std::{
+    collections::HashMap,
+    ffi::OsStr,
+    path::{Path, PathBuf},
+};
 
 use glyphslib::Font;
-use norad::designspace::DesignSpaceDocument;
-use norad::Font as UfoFont;
+use norad::{designspace::DesignSpaceDocument, Font as UfoFont};
 
-use crate::FontspectorError;
+use crate::{prelude::FixFnResult, FontspectorError};
+
+#[derive(Debug, Clone)]
+/// Our representation of a DesignSpace document, which contains multiple UFO sources.
+pub struct DesignSpace {
+    /// The DesignSpace document.
+    pub document: DesignSpaceDocument,
+    /// The source fonts
+    pub sources: HashMap<String, Box<UfoFont>>,
+}
+
+impl DesignSpace {
+    /// Load a DesignSpace document from a path.
+    pub fn load(path: &Path) -> Result<Self, FontspectorError> {
+        let document = DesignSpaceDocument::load(path)?;
+        let mut sources = HashMap::new();
+        for source in &document.sources {
+            let ufo_font = UfoFont::load(path.with_file_name(&source.filename))?;
+            sources.insert(source.filename.clone(), Box::new(ufo_font));
+        }
+        Ok(Self { document, sources })
+    }
+
+    /// Save the DesignSpace document to a path.
+    pub fn save(&self, path: &Path) -> Result<(), FontspectorError> {
+        self.document
+            .save(path)
+            .map_err(|e| FontspectorError::SaveError {
+                path: path.to_path_buf(),
+                error: e.to_string(),
+            })?;
+        // Now save all the UFO sources
+        for (name, font) in &self.sources {
+            font.save(path.with_file_name(name))?;
+        }
+        Ok(())
+    }
+
+    /// Apply to fix to each UFO source in the DesignSpace document.
+    pub fn apply_fix(&mut self, fix: &dyn Fn(&mut UfoFont) -> FixFnResult) -> FixFnResult {
+        let mut changed = false;
+        for font in self.sources.values_mut() {
+            if fix(font)? {
+                changed = true;
+            }
+        }
+        Ok(changed)
+    }
+}
 
 /// A source of a font, which can be a Glyphs file, a UFO file, or a DesignSpace document.
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub enum Source {
     /// A source that contains a Glyphs font.
     ///
@@ -17,7 +67,7 @@ pub enum Source {
     /// A source that contains a UFO font.
     Ufo(Box<UfoFont>),
     /// A source that contains a DesignSpace document.
-    Designspace(DesignSpaceDocument),
+    Designspace(Box<DesignSpace>),
 }
 
 /// A source file that can be loaded (and saved) from a path.
@@ -39,7 +89,7 @@ impl SourceFile {
             .and_then(OsStr::to_str)
             .ok_or_else(|| FontspectorError::UnrecognizedSource(path.to_path_buf()))?;
         let source = match ext {
-            "designspace" => Ok(Source::Designspace(DesignSpaceDocument::load(path)?)),
+            "designspace" => Ok(Source::Designspace(Box::new(DesignSpace::load(path)?))),
             "ufo" => Ok(Source::Ufo(Box::new(UfoFont::load(path)?))),
             "glyphs" | "glyphspackage" => Ok(Source::Glyphs(Box::new(Font::load(path)?))),
             _ => Err(FontspectorError::UnrecognizedSource(path.to_path_buf())),
@@ -69,12 +119,7 @@ impl SourceFile {
                         error: e.to_string(),
                     })
             }
-            Source::Ufo(font) => font
-                .save(&self.file)
-                .map_err(|e| FontspectorError::SaveError {
-                    path: self.file.clone(),
-                    error: e.to_string(),
-                }),
+            Source::Ufo(font) => Ok(font.save(&self.file)?),
             Source::Designspace(doc) => {
                 doc.save(&self.file)
                     .map_err(|e| FontspectorError::SaveError {
