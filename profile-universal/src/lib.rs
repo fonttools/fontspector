36399a0f334f5348bf7c5dc951c3d03fb1fe0afe
Simon Cozens <simon@simon-cozens.org>
2025-09-09 14:42:05 +0100

    feat(opentype/has_glyphs): New check (#417)

--- profile-universal/src/lib.rs
+++ profile-universal/src/lib.rs
@@ -94,6 +94,7 @@ impl fontspector_checkapi::Plugin for Universal {
             .add_and_register_check(checks::whitespace_widths)
             .add_and_register_check(checks::field_values)
             .add_and_register_check(checks::has_features)
+            .add_and_register_check(checks::has_glyphs)
             .build("universal", cr)
 
         //  Checks which don't make sense any more
