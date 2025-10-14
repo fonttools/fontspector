a059bae9570e98cffe2f6596edb752feba568731
Simon Cozens <simon@simon-cozens.org>
2025-10-14 16:11:31 +0100

    fix(arabic_high_hamza): Fix typo in codepoint (#494)
    * fix(arabic_high_hamza): Typo in high hamza codepoint
    
    * test(arabic_high_hamza): Add tests

--- profile-universal/src/checks/arabic_high_hamza.rs
+++ profile-universal/src/checks/arabic_high_hamza.rs
@@ -5,16 +5,16 @@ use fontspector_checkapi::{
 };
 
 const ARABIC_LETTER_HAMZA: u32 = 0x0621;
-const ARABIC_LETTER_HIGH_HAMZA: u32 = 0x0675;
+const ARABIC_LETTER_HIGH_HAMZA: u32 = 0x0674;
 
 #[check(
     id = "arabic_high_hamza",
-    title = "Check that glyph for U+0675 ARABIC LETTER HIGH HAMZA is not a mark.",
+    title = "Check that glyph for U+0674 ARABIC LETTER HIGH HAMZA is not a mark.",
     rationale = "
-        Many fonts incorrectly treat ARABIC LETTER HIGH HAMZA (U+0675) as a variant of
+        Many fonts incorrectly treat ARABIC LETTER HIGH HAMZA (U+0674) as a variant of
         ARABIC HAMZA ABOVE (U+0654) and make it a combining mark of the same size.
 
-        But U+0675 is a base letter and should be a variant of ARABIC LETTER HAMZA
+        But U+0674 is a base letter and should be a variant of ARABIC LETTER HAMZA
         (U+0621) but raised slightly above baseline.
 
         Not doing so effectively makes the font useless for Jawi and
@@ -30,7 +30,7 @@ fn arabic_high_hamza(t: &Testable, context: &Context) -> CheckFnResult {
         !codepoints.contains(&ARABIC_LETTER_HIGH_HAMZA)
             || !codepoints.contains(&ARABIC_LETTER_HAMZA),
         "glyphs-missing",
-        "This check will only run on fonts that have both glyphs U+0621 and U+0675"
+        "This check will only run on fonts that have both glyphs U+0621 and U+0674"
     );
 
     #[allow(clippy::unwrap_used)] // We just tested for it
@@ -57,9 +57,49 @@ fn arabic_high_hamza(t: &Testable, context: &Context) -> CheckFnResult {
     if ((high_hamza_area - hamza_area) / hamza_area).abs() > 0.1 {
         problems.push(Status::fail(
             "glyph-area",
-            "The arabic letter high hamza (U+0675) should have roughly the same size the arabic letter hamza (U+0621), but a different glyph outline area was detected.",
+            "The arabic letter high hamza (U+0674) should have roughly the same size the arabic letter hamza (U+0621), but a different glyph outline area was detected.",
         ))
     }
 
     return_result(problems)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use fontspector_checkapi::codetesting::{
+        assert_pass, assert_results_contain, remap_glyph, run_check, test_able,
+    };
+
+    use fontspector_checkapi::StatusCode;
+
+    #[allow(clippy::expect_used)]
+    #[test]
+    fn test_arabic_high_hamza() {
+        let testable = test_able("notosansarabic/NotoSansArabic-Regular.ttf");
+        let results = run_check(arabic_high_hamza, testable);
+        assert_pass(&results);
+
+        // Should skip on a non-Arabic font
+        let testable = test_able("nunito/Nunito-Regular.ttf");
+        let results = run_check(arabic_high_hamza, testable);
+        assert_results_contain(
+            &results,
+            StatusCode::Skip,
+            Some("glyphs-missing".to_string()),
+        );
+
+        // Remap high hamza to a mark glyph, damma will do
+        let mut testable = test_able("notosansarabic/NotoSansArabic-Regular.ttf");
+        remap_glyph(&mut testable, ARABIC_LETTER_HIGH_HAMZA, "uni064F").expect("remap failed");
+        let results = run_check(arabic_high_hamza, testable);
+        assert_results_contain(&results, StatusCode::Fail, Some("mark-in-gdef".to_string()));
+
+        // Remap high hamza to a small base glyph, use period
+        let mut testable = test_able("notosansarabic/NotoSansArabic-Regular.ttf");
+        remap_glyph(&mut testable, ARABIC_LETTER_HIGH_HAMZA, "period").expect("remap failed");
+        let results = run_check(arabic_high_hamza, testable);
+        assert_results_contain(&results, StatusCode::Fail, Some("glyph-area".to_string()));
+    }
 }
