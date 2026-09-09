178f989dea5ccb0a2853fe9e32f7f6b270196b67
Simon Cozens <simon@simon-cozens.org>
2026-09-09 13:15:40 +0000

    chore: Minimize the crates required by the hotfix crate (#905)
    * chore: Minimize the crates required by the hotfix crate
    
    * chore: Update fontc deps

--- profile-googlefonts/src/checks/mod.rs
+++ profile-googlefonts/src/checks/mod.rs
@@ -1,9 +1,12 @@
 mod dotted_circle;
 pub mod googlefonts;
+#[cfg(feature = "check")]
 pub mod outline;
 pub use dotted_circle::dotted_circle;
-#[cfg(not(target_family = "wasm"))]
+#[cfg(all(feature = "check", not(target_family = "wasm")))]
 // Because it needs to find *.json. XXX Rewrite to use collections
 pub mod shaping;
+#[cfg(feature = "check")]
 mod soft_dotted;
+#[cfg(feature = "check")]
 pub use soft_dotted::soft_dotted;
